484c403dd18627400d7a6e25e3951c0252fd1d04
Johan <johan8838373@gmail.com>
2023-12-29 17:02:43 -0500

    init proyect

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./*.html', './src/**/*.{html,js}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			raleway: ['Raleway, sans-serif'],
+		},
+	},
+	plugins: [],
+}
